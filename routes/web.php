8af77e20dd213c37702745a1b8f6a723ae6dfef7
yuming <18810448544@163.com>
2018-01-19 14:31:33 +0800

    user profile page

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -19,24 +8,24 @@
 Route::get('/','PagesController@root')->name('root');
 
 // Auth::routes();
-// Authentication Routes...
+// 登錄路由
 Route::get('login','Auth\LoginController@showLoginForm')->name('login');
 Route::post('login','Auth\LoginController@login');
 Route::post('logout','Auth\LoginController@logout')->name('logout');
 
 
 
-// Registration Routes...
+// 註冊路由
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
 
 
 
-// Password Reset Routes
+// 密碼重置路由
 Route::get('password/reset','Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-
 Route::post('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-
 Route::get('password/reset/{token}','Auth\ResetPasswordController@showResetForm')->name('password.reset');
-
 Route::post('password.reset','Auth\ResetPasswordController@reset');
+
+// 用戶路由
+Route::resource('users','UsersController',['only'=>['show','update','edit']]);
