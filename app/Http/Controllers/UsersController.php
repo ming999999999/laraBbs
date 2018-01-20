4a429fe18904a5f078bb2dc6b8bf53cf6a5f54f7
yuming <18810448544@163.com>
2018-01-20 10:34:59 +0800

    add user cate

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,12 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
     	return view('users.show',compact('user'));
@@ -17,6 +23,8 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
+        
     	return view('users.edit',compact('user'));
 
     }
@@ -24,6 +32,8 @@ class UsersController extends Controller
 
     public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
     {
+
+        $this->authorize('update', $user);
 
         $data = $request->all();
 
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ Route::get('login','Auth\LoginController@showLoginForm')->name('login');
 Route::post('login','Auth\LoginController@login');
 Route::post('logout','Auth\LoginController@logout')->name('logout');
 
-
-
 // 註冊路由
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
