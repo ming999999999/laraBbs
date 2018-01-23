1f1cae604f7de6c997b520f09bfaa62f82b62a61
yuming <18810448544@163.com>
2018-01-23 11:57:18 +0800

    user can edit they own topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,14 +7,16 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
-    public function update(User $user, Topic $topic)
-    {
-        // return $topic->user_id == $user->id;
-        return true;
-    }
+    
 
     public function destroy(User $user, Topic $topic)
     {
         return true;
+    }
+
+    public function update(User $user,Topic $topic)
+    {
+
+    	return $topic->user_id == $user->id;
     }
 }
