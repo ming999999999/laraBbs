e98d4c1b3f7bc2014b103b0b41d401e70c71a0ec
yuming <18810448544@163.com>
2018-01-23 09:05:10 +0800

    user can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,8 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 // 分类列表话题
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+
+// 图片上传
+Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
+
