31f81e735465c4098e8da599e1994f1e26ac954a
yuming <18810448544@163.com>
2018-01-27 11:14:27 +0800

    admin can manage content

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -12,3 +12,22 @@
 
 		return str_limit($excerpt,$length);
 	}
+
+
+	function model_admin_link($title,$model)
+	{
+		return model_link($title,$model,'admin');
+	}
+
+	function model_link($title,$model,$prefix='')
+	{
+
+		// 获取数据模型的复数蛇形命名
+		$model_name = model_plural_name($model);
+
+		// 初始化前缀
+		$prefix = $prefix?"/$prefix/":'/';
+
+		// 使用站点的url拼接全量url
+		$url = config('app.url').$prefix.$model_name.'/'.$model->id;
+	}
