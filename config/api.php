c59fd3eeadbec5d241698bdd562234e9bb578ee7
yuming <18810448544@163.com>
2018-02-05 10:01:58 +0800

    rate limits

--- config/api.php
+++ config/api.php
@@ -238,13 +238,13 @@ return [
         // 访问频率的限制,次数/分钟
         'access'=>[
             'expires'=>env('RATE_LIMITS_EXPIRES',1),
-            'LIMIT'=>env('RATE_LIMITS',60),
+            'limit'=>env('RATE_LIMITS',60),
         ],
 
         // 登录相关,次数/分钟
         'sign'=>[
             'expires'=>env('SIGN_RATE_LIMITS_EXPIRES',1),
-            'LIMIT'=>env('SIGN_RATE_LIMITS',10),
+            'limit'=>env('SIGN_RATE_LIMITS',10),
         ],
     ],
 
